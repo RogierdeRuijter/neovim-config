fca0e27ef955ce5ba6b3c5ecd796661fccdbffec
Rogier de Ruijter <r.de.ruijter@vpro.nl>
2024-10-01 22:18:14 +0200

    update remaps

--- lua/rogier/remap.lua
+++ lua/rogier/remap.lua
@@ -1,15 +1,15 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>.", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "N", "Nzzzv")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
 vim.keymap.set("n", "<leader>vwm", function()
   require("vim-with-me").StartVimWithMe()
 end)
@@ -33,8 +33,9 @@ vim.keymap.set("n", "<C-s>", ":w<CR>")
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set('n', '<C-o>', ':silent !tmux new-window<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ot', ':silent !tmux new-window<CR>', { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
 -- add 2 new panes on the right and focus on the top one
 vim.api.nvim_set_keymap('n', '<leader>[', ':silent !tmux split-window -h \\; split-window -v \\; select-pane -U<CR>',
   { noremap = true, silent = true })
@@ -48,12 +49,13 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+-- TODO: figure out how to use this
+-- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+-- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
 
 -- remap leader o to closing bracket
-vim.api.nvim_set_keymap('n', '<Leader>o', 'o}<Esc>k0', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>o', 'o}<Esc>k0', { noremap = true, silent = true })
